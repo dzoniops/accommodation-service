b4b9b22a9f8e061cf9f62e827d0a4276cb36f3ca
Nikola Petrovic <s.nino.petrovic@gmail.com>
2023-07-22 16:26:17 +0200

    feat: changed Reservation and Payment Type to match proto file

--- models/acccommodation.go
+++ models/acccommodation.go
@@ -6,36 +6,38 @@ import (
 
 type Accommodation struct {
 	gorm.Model
-	ID               int64                `json:"id"`
-	HostID           int64                `json:"hostId"`
+	ID               int64                `json:"id"                gorm:"primaryKey"`
+	HostID           int64                `json:"host_id"`
 	Name             string               `json:"name"`
 	Town             string               `json:"town"`
 	Municipality     string               `json:"municipality"`
 	Country          string               `json:"country"`
 	Amenities        string               `json:"amenities"`
-	MinGuests        int                  `json:"minGuests"`
-	MaxGuests        int                  `json:"maxGuests"`
-	PricingModel     PricingModel         `json:"pricingModel"`
-	ReservationModel ReservationModel     `json:"reservationModel"`
+	MinGuests        int                  `json:"min_guests"`
+	MaxGuests        int                  `json:"max_guests"`
+	PricingModel     PricingModel         `json:"pricing_model"`
+	ReservationModel ReservationModel     `json:"reservation_model"`
 	Images           []AccommodationImage `json:"images"`
 }
 
-type PricingModel string
+type PricingModel int32
 
 const (
-	PUPN PricingModel = "pupn"
-	PGPN PricingModel = "pgpn"
+	PRICING_MODEL_UNSPECIFIED PricingModel = 0
+	PUPN                      PricingModel = 1
+	PGPN                      PricingModel = 2
 )
 
-type ReservationModel string
+type ReservationModel int32
 
 const (
-	AUTO   ReservationModel = "auto"
-	MANUAL ReservationModel = "manual"
+	RESERVATION_MODEL_UNSPECIFIED ReservationModel = 0
+	AUTO                          ReservationModel = 1
+	MANUAL                        ReservationModel = 2
 )
 
 type AccommodationImage struct {
 	ID              int64  `json:"id"`
-	B64IMG          string `json:"b64img" gorm:"not null"`
+	B64IMG          string `json:"b64_img"       gorm:"not null"`
 	AccommodationID int64  `json:"accommodation" gorm:"not null"`
 }
