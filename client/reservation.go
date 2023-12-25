7609f5a1f54508669d9f8484f5571d2b0da2d9fe
Nikola Petrovic <nikola.petrovic@panonit.com>
2023-12-25 15:30:34 +0100

    fix: wrong name for function

--- client/reservation.go
+++ client/reservation.go
@@ -11,7 +11,7 @@ type ReservationClient struct {
 	client pb.ReservationServiceClient
 }
 
-func InitClient(url string) *ReservationClient {
+func InitReservationClient(url string) *ReservationClient {
 	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		fmt.Println("Could not connect:", err)
